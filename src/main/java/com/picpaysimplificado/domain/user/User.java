a32b364bd7b80c53b15dbb441329efeb00a98c83
fsousac <felipescoelho517@gmail.com>
2023-08-27 09:37:00 -0300

    Feat: Criando o dominio da aplicação e os repositórios de queries

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,13 +1,36 @@
 package com.picpaysimplificado.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.table;
-@Entity(name="users")
-@Table(name="users")
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
 @Getter
 @Setter
-@AllAgrsConstructor
-@EqualsAndHashCode(of="id")
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    public User() {
+
+    }
 }
