3fcb3689c4481868fb8925f7a3b5ee340eb1766e
fsousac <felipescoelho517@gmail.com>
2023-08-28 20:00:37 -0300

    feat: create TransationRepository

--- src/main/java/com/picpaysimplificado/respositories/TransationRepository.java
+++ src/main/java/com/picpaysimplificado/respositories/TransationRepository.java
@@ -1,6 +1,6 @@
 package com.picpaysimplificado.respositories;
 
-import jakarta.transaction.Transaction;
+import com.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransationRepository extends JpaRepository<Transaction, Long> {
